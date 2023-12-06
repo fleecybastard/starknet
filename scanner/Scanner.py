1b02b34b68383c609b0bb31967b0210573734e61
fleecybastard <illia09092003@gmail.com>
2023-12-06 13:31:39 +0200

    updated scanner

--- scanner/Scanner.py
+++ scanner/Scanner.py
@@ -7,6 +7,9 @@ import aiohttp
 
 
 class Scanner:
+
+    MAX_RETRIES = 10
+
     URL = 'https://voyager.online/api'
 
     def __init__(self):
@@ -18,12 +21,17 @@ class Scanner:
     async def close(self) -> None:
         await self.session.close()
 
-    async def _request(self, method: str, path: str, params: dict = None, data: dict = None) -> dict:
-        response = await self.session.request(method=method,
-                                              url=f'{self.URL}{path}',
-                                              params=params,
-                                              data=data)
-        return await response.json()
+    async def _request(self, method: str, path: str, params: dict = None, data: dict = None) -> dict | None:
+        for _ in range(self.MAX_RETRIES):
+            try:
+                response = await self.session.request(method=method,
+                                                  url=f'{self.URL}{path}',
+                                                  params=params,
+                                                  data=data)
+
+                return await response.json()
+            except:
+                pass
 
     async def _get_transactions(self, address: str) -> dict:
         params = {
@@ -35,14 +43,18 @@ class Scanner:
 
     async def get_transactions_count(self, address: str) -> int:
         transactions = await self._get_transactions(address)
+        if transactions is None:
+            return 0
         count = 0
-        for transaction in transactions['items']:
+        for transaction in transactions.get('items', []):
             if transaction['execution_status'] == 'Succeeded':
                 count += 1
         return count
 
     async def get_eth_balance(self, address: str) -> Decimal:
         response = await self._request('GET', f'/contract/{address}/balances')
+        if response is None:
+            response = []
         for balance in response:
             if balance['name'] == 'Ether':
                 return Decimal(balance['balance'])
