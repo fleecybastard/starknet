85f63909629f91ee1f530cbda2bf4a7b7979adf6
fleecybastard <illia09092003@gmail.com>
2024-01-25 03:17:11 +0200

    fixed gas price

--- utils/ensure_low_gas.py
+++ utils/ensure_low_gas.py
@@ -1,7 +1,7 @@
 import asyncio
 
 from web3 import AsyncWeb3
-from starknet_py.net.gateway_client import GatewayClient
+from starknet_py.net.full_node_client import FullNodeClient
 
 from utils.sleep import random_sleep_time
 
@@ -15,9 +15,9 @@ async def get_current_eth_gwei():
 
 async def get_current_starknet_gwei():
     try:
-        gateway_client = GatewayClient(net='mainnet')
-        block = await gateway_client.get_block(block_number='pending')
-        return block.gas_price / 10 ** 9
+        client = FullNodeClient('https://starknet-mainnet.public.blastapi.io')
+        block = await client.get_block(block_number='pending')
+        return block.l1_gas_price.price_in_wei / 10 ** 9
     except:
         return float('inf')
 
